663e25b45337e87995c91a7ee462493df950e486
M7x0-0ya2ser <m.yasser.200300@gmail.com>
2025-04-13 20:29:22 +0200

    add 7amata connection

--- BookSwap/Program.cs
+++ BookSwap/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<BookSwapDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("myCon")));
+builder.Services.AddDbContext<BookSwapDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("7amataCon")));
 
 var app = builder.Build();
 
