d207735393824182baa006a4c5ff852fa9f3181c
Mahmoud Elnahrawy <mahmoudelnahrawy02@gmail.com>
2025-04-14 17:57:04 +0200

    add na7o con

--- BookSwap/Program.cs
+++ BookSwap/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<BookSwapDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("7amataCon")));
+builder.Services.AddDbContext<BookSwapDbContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("nahrawcon")));
 
 var app = builder.Build();
 
